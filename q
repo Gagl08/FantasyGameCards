8771e3f1fdaaaa00ec2739f11039418f7e7b1dc7
safoualo <mrzksfn@yahoo.fr>
2023-11-21 11:15:12 +0100

    Updated Building.java

---
+++ q
@@ -0,0 +1,29 @@
+[33mcommit dea9395d051b1530e2bc578a2cdf39e010656452[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m, [m[1;31morigin/master[m[33m, [m[1;31morigin/HEAD[m[33m)[m
+Author: safoualo <mrzksfn@yahoo.fr>
+Date:   Tue Nov 21 09:38:32 2023 +0100
+
+    Added new classes
+
+[33mcommit 3e766653452765c38e23089ad3b79c04873f4fbf[m
+Author: burgu <gael.burgues@gmail.com>
+Date:   Tue Nov 21 08:28:36 2023 +0100
+
+    Creation structure
+
+[33mcommit a3c07f0e0b4965b2f3e7f23c471e43032d24771b[m
+Author: burgu <gael.burgues@gmail.com>
+Date:   Tue Nov 21 08:19:44 2023 +0100
+
+    Creation structure
+
+[33mcommit 0247b6c7b88134f49d8abf4b350bb720471f833f[m
+Author: Gagl08 <62640192+Gagl08@users.noreply.github.com>
+Date:   Tue Nov 21 08:18:06 2023 +0100
+
+    Create README.md
+
+[33mcommit 0e217082dae4d097973c4119bf1fa240fe65f05c[m
+Author: burgu <gael.burgues@gmail.com>
+Date:   Tue Nov 21 08:16:01 2023 +0100
+
+    commit initial
--- src/fr/bordeaux/depInfo/projetAO/Building.java
+++ src/fr/bordeaux/depInfo/projetAO/Building.java
@@ -1,4 +1,169 @@
 package fr.bordeaux.depInfo.projetAO;
 
 public class Building {
+    // The name of the building.
+    final String name;
+    int nbHabAllowed;
+    int nbWorkerNeeded;
+    Ressources resConso;
+    Ressources resProd;
+    boolean active;
+    int timerBuild;
+
+//////////////////GETTERS and SETTERS///////////////////
+    /**
+     * Retrieves the name of the building.
+     *
+     * @return The name of the building.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Retrieves the number of inhabitants needed for the building.
+     *
+     * @return The number of inhabitants needed.
+     */
+    public int getNbHabAllowed() {
+        return nbHabAllowed;
+    }
+
+    /**
+     * Sets the number of inhabitants needed for the building.
+     *
+     * @param nbHabAllowed The new number of inhabitants needed.
+     */
+    public void setNbHabAllowed(int nbHabAllowed) {
+        this.nbHabAllowed = nbHabAllowed;
+    }
+
+    /**
+     * Retrieves the number of workers needed for the building.
+     *
+     * @return The number of workers needed.
+     */
+    public int getNbWorkerNeeded() {
+        return nbWorkerNeeded;
+    }
+
+    /**
+     * Sets the number of workers needed for the building.
+     *
+     * @param nbWorkerNeeded The new number of workers needed.
+     */
+    public void setNbWorkerNeeded(int nbWorkerNeeded) {
+        this.nbWorkerNeeded = nbWorkerNeeded;
+    }
+
+    /**
+     * Retrieves the resources consumed by the building.
+     *
+     * @return The resources consumed.
+     */
+    public Ressources getResConso() {
+        return resConso;
+    }
+
+    /**
+     * Sets the resources consumed by the building.
+     *
+     * @param resConso The new resources consumed.
+     */
+    public void setResConso(Ressources resConso) {
+        this.resConso = resConso;
+    }
+
+    /**
+     * Retrieves the resources produced by the building.
+     *
+     * @return The resources produced.
+     */
+    public Ressources getResProd() {
+        return resProd;
+    }
+
+    /**
+     * Sets the resources produced by the building.
+     *
+     * @param resProd The new resources produced.
+     */
+    public void setResProd(Ressources resProd) {
+        this.resProd = resProd;
+    }
+
+    /**
+     * Checks if the building is currently active and has no workers needed.
+     *
+     * @return True if the building is functional, false otherwise.
+     */
+    public boolean isActive() {
+        return active;
+    }
+
+    /**
+     * Sets the activation status of the building.
+     *
+     * @param active The new activation status.
+     */
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    /**
+     * Retrieves the time required to build the building.
+     *
+     * @return The time required for building.
+     */
+    public int getTimerBuild() {
+        return timerBuild;
+    }
+
+    /**
+     * Sets the time required to build the building.
+     *
+     * @param timerBuild The new time required for building.
+     */
+    public void setTimerBuild(int timerBuild) {
+        this.timerBuild = timerBuild;
+    }
+
+////////////////////////////////////////////////////////
+/////////////////////CONSTRUCTORS///////////////////////
+    public Building(String name, int nbWorkerNeeded, int timerBuild) {
+        this.name = name;
+        this.nbHabAllowed = 0;
+        this.nbWorkerNeeded = nbWorkerNeeded;
+        this.active = false;
+        this.timerBuild = timerBuild;
+    }
+////////////////////////////////////////////////////////
+/////////////////////METHODS////////////////////////////
+    /**
+     * Checks if the building is functional, i.e if the player activated the card
+     * and if the building has enough workers to run it.
+     * @return True if the building is functional, false otherwise.
+     */
+    public boolean isFunctional() {
+        return this.active && this.nbWorkerNeeded == 0;
+    }
+
+    /**
+     * Produces resources for the given player hand (placeholder method, needs implementation).
+     *
+     * @param player The player hand for which resources are produced.
+     */
+    public void produceRessource(PlayerHand player) {
+        // Implementation for producing resources
+    }
+
+    /**
+     * Consumes resources from the given player hand (placeholder method, needs implementation).
+     *
+     * @param player The player hand from which resources are consumed.
+     */
+    public void consumeRessource(PlayerHand player) {
+        // Implementation for consuming resources
+    }
+////////////////////////////////////////////////////////
 }
