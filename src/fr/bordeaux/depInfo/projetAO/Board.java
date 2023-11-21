1bb18365def162fbdd39520480d8c07486f3b420
safoualo <mrzksfn@yahoo.fr>
2023-11-21 11:37:04 +0100

    Updated Board.java

--- src/fr/bordeaux/depInfo/projetAO/Board.java
+++ src/fr/bordeaux/depInfo/projetAO/Board.java
@@ -6,5 +6,32 @@
 
 package fr.bordeaux.depInfo.projetAO;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Board game is where the player places the cards, it is the "map" of the game.
+ */
 public class Board {
+    List<Card> cardsPlayed;
+
+    public Board(){
+        this.cardsPlayed = new ArrayList<Card>();
+    }
+
+    public void addCard(Card card){
+        this.cardsPlayed.add(card);
+    }
+    public void removeCard(Card card){
+        this.cardsPlayed.remove(card);
+    }
+
+    public void deactivateBuilding(int indexCard){
+        this.cardsPlayed.get(indexCard).setActive(false);
+    }
+    public void activateBuilding(int indexCard){
+        this.cardsPlayed.get(indexCard).setActive(true);
+    }
+
+
 }
