991fc9251a688308a0ba7e849256cd3dc4e4161a
burgu <gael.burgues@gmail.com>
2023-11-21 11:40:31 +0100

    Start Player Hand

--- src/fr/bordeaux/depInfo/projetAO/PlayerHand.java
+++ src/fr/bordeaux/depInfo/projetAO/PlayerHand.java
@@ -6,5 +6,24 @@
 
 package fr.bordeaux.depInfo.projetAO;
 
+import java.util.ArrayList;
+
+/**
+ * Player Hand
+ */
 public class PlayerHand {
+    //General Ressources use by the player
+    Ressources general_Ressources;
+    //Carte playable of the player
+    ArrayList <Building> hand = new ArrayList<Building>();
+
+    public void playCard(Card card, Board board){
+        board.addCard(card);
+        for (int i = 0; i<hand.size();i++){
+            if(hand.get(i).equals(card)){
+                hand.remove(i);
+            }
+        }
+    }
+
 }
